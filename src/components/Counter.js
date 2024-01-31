570656eaa757d886ce11d8e9977015a8182ae38b
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-01-31 19:10:38 +0300

    Reviewed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,12 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    console.log(`before setState: ${count}`);
+
+    setCount(c => c + 1);
+    setCount(c => c + 1);
+
+    console.log(`after setState: ${count}`);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
